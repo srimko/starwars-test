722975d5a669de0f8ecd26335009f03c89e0aac9
Alexandre Cédrick <alexandre.cedrick@gmail.com>
2021-09-01 14:56:50 +0200

    Responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,29 @@
 import simpleParallax from 'simple-parallax-js'
 
 window.addEventListener('DOMContentLoaded', () => {
+
+  // Burger menu
+  const menuBurger = document.querySelector('.js-menuBurger')
+  const closeBtn = document.querySelector('.js-close')
+  const sideNav = document.querySelector('.sidenav')
+
+  menuBurger.addEventListener('click', event => {
+    if (sideNav) {
+      sideNav.classList.add('active')
+    }
+  })
+  closeBtn.addEventListener('click', event => {
+    if (sideNav) {
+      sideNav.classList.remove('active')
+    }
+  })
+
+  // Parallax
   const image = document.querySelectorAll('.js-parallax')
 
-  new simpleParallax(image, {
-    scale: 1.8
-  })
+  if (image) {
+    new simpleParallax(image, {
+      scale: 1.8
+    })
+  }
 })
